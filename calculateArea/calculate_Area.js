26d44d97533f7aed4ee8a2b37ccef3b1e07def2b
helene-ploub <hele.masse@orange.com>
2025-02-14 12:21:39 +0100

    followers count

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -5,9 +5,4 @@ function calculateArea() {
  width = parseFloat(document.getElementById('width').value);
  let area = length * width;
  document.getElementById('result').innerText = The area of the rectangle is: ${area};
-
-
-
-
-
 }
